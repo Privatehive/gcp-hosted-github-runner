88303fedea8701daf755499bb9df94c27ba1b5c4
Björn Stresing <bjoern.stresing@gmx.de>
2024-07-31 01:16:57 +0200

    Use random_string instead of random_password for cloud task queue suffix

--- tasks.tf
+++ tasks.tf
@@ -1,15 +1,17 @@
-resource "random_password" "task_queue_suffix" {
+resource "random_string" "task_queue_suffix" {
   length  = 5
-  upper = false
+  upper   = false
+  special = false
+  numeric = false
 }
 
 resource "google_cloud_tasks_queue" "autoscaler_tasks" {
-  name       = "autoscaler-callback-queue-${random_password.task_queue_suffix.result}"
+  name       = "autoscaler-callback-queue-${random_string.task_queue_suffix.result}"
   location   = local.region
   depends_on = [google_project_service.cloudtasks_api]
 
   retry_config {
-    max_attempts       = 10 // max_attempts && max_retry_duration have to be fullfilled to stop retires
+    max_attempts       = 10      // max_attempts && max_retry_duration have to be fullfilled to stop retires
     max_retry_duration = "3600s" // max_attempts && max_retry_duration have to be fullfilled to stop retires
     max_backoff        = "600s"
     min_backoff        = "60s"
