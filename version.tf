6a83c771574832206091d00f9ab72d0f30e60928
Björn Stresing <bjoern.stresing@gmx.de>
2025-05-18 18:10:56 +0200

    Increment version of autoscaler

--- version.tf
+++ version.tf
@@ -1,4 +1,4 @@
 # used to tag the docker image of autoscaler. Should be incremented every time the autoscaler code is changed.
 locals {
-  autoscaler_version = "1.0.1-snapshot"
+  autoscaler_version = "1.1"
 }
